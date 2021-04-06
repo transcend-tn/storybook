54748f04eff26805c30228c2d10f0c33cf2395a3
Wassim Arfa <wassim.arfa@gmail.com>
2021-04-06 11:22:44 +0100

    Tailwind Setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles/index.css"; // <- applied everywhere in the NextJS application scope
 
 const MyApp = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
