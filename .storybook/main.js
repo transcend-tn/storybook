d2d600f22ad135bc3a860350500a091f43268848
Wassim Arfa <wassim.arfa@gmail.com>
2021-04-09 11:25:53 +0100

    Setup Jest
    npm install --save-dev @storybook/addon-jest jest "@types/jest" ts-jest
    npm run test:generate-output -- --watch

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,5 +2,6 @@ const path = require("path");
 
 module.exports = {
   stories: ["../components/**/**/*.stories.@(tsx|mdx)"],
-  addons: ['@storybook/addon-essentials'],
+  addons: ['@storybook/addon-essentials',
+  '@storybook/addon-jest'],
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -5,3 +5,13 @@
 // in the context of our stories
 
 import "../styles/index.css";
+import { addDecorator } from '@storybook/react'; // <- or your view layer
+import { withTests } from '@storybook/addon-jest';
+
+import results from '../.jest-test-results.json';
+
+addDecorator(
+  withTests({
+    results,
+  })
+);
