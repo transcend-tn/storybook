9b77f8f04fce1eba9da7b6ade495e5da76e94789
Wassim Arfa <wassim.arfa@gmail.com>
2021-09-02 22:23:34 +0100

    fix: feedback

---
+++ components/Map/ContactMap.tsx
@@ -0,0 +1,27 @@
+import GoogleMapReact from 'google-map-react';
+import MapMarker from './MapMarker';
+
+interface contactMapProps {
+  center: {
+    lat: number;
+    lng: number;
+  };
+  zoom: number;
+}
+export default function ContactMap({
+  center = { lat: 35.83679602257312, lng: 10.63148022053524 },
+  zoom = 18.2,
+}: contactMapProps): JSX.Element {
+  return (
+    // Important! Always set the container height explicitly
+    <div className="h-full w-full">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: '' }}
+        defaultCenter={center}
+        defaultZoom={zoom}
+      >
+        <MapMarker text="Transcend" />
+      </GoogleMapReact>
+    </div>
+  );
+}
